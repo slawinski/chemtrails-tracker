0d62768d5632b87f61cd5bb8aedd22892756d01c
piotr-s <piotr.slawinski1@gmail.com>
2019-02-15 06:45:36 +0100

    Add icon author credit disclaimer

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="nav">
     <h1>{{ msg }}</h1>
+    <p>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></p>
   </div>
 </template>
 
