e56a6fe10536d92818affd536cfa186661744ef5
piotr-s <piotr.slawinski1@gmail.com>
2019-10-08 20:01:51 +0200

    Resize marker icons and enable rotation

--- src/components/MapMarker.vue
+++ src/components/MapMarker.vue
@@ -1,50 +1,31 @@
 <template>
   <div>
-    <l-marker :lat-lng="marker" />
+    <l-rotated-marker
+      :lat-lng="this.flight.position"
+      :rotationAngle="this.flight.trueTrack"
+    >
+      <l-icon>
+        <img src="../../src/assets/airplane.svg" alt="asdf" />
+      </l-icon>
+    </l-rotated-marker>
   </div>
 </template>
 
 <script>
-import svgPlane from '!raw-loader!../assets/airplane.svg';
-import { LMarker } from 'vue2-leaflet';
-import { latLng } from 'leaflet';
+import { LIcon } from 'vue2-leaflet';
+import Vue2LeafletRotatedMarker from 'vue2-leaflet-rotatedmarker';
 
 export default {
   name: 'MapMarker',
-  components: { LMarker },
+  components: {
+    LIcon,
+    'l-rotated-marker': Vue2LeafletRotatedMarker,
+  },
   props: {
     flight: {
       type: Object,
       required: true,
     },
-  },
-  data() {
-    return {
-      marker: latLng(52, 19),
-    };
-  },
-  mounted() {
-    const parser = new DOMParser();
-    const svgPath = parser
-      .parseFromString(svgPlane, 'image/svg+xml')
-      .querySelector('path')
-      .getAttribute('d');
-
-    new this.google.maps.Marker({
-      position: this.flight.position,
-      flight: this.flight,
-      map: this.map,
-      // TODO: extract to object
-      icon: {
-        path: svgPath,
-        scale: 0.04,
-        fillColor: '#FFFFFF',
-        fillOpacity: 1,
-        strokeWeight: 1,
-        rotation: this.flight.trueTrack,
-        anchor: new this.google.maps.Point(250, 400),
-      },
-    });
   },
 };
 </script>
