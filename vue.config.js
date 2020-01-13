007eced6c5e63c42d97ddc21fca63c9ad928ee52
piotr-s <piotr.slawinski1@gmail.com>
2020-01-13 20:14:31 +0100

    Fix issue with dependencies conflict for vue2-leaflet plugins

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
+const path = require('path');
+
 module.exports = {
   devServer: {
     proxy: 'https://opensky-network.org/',
+  },
+  /*
+   * Issue with peer dependencies required to make an alias to point to a single
+   * leaflet root instance as in https://github.com/jurb/vue2-leaflet-heatmap/issues/1
+   */
+  configureWebpack: {
+    resolve: {
+      alias: {
+        leaflet: path.join(__dirname, 'node_modules/leaflet'),
+      },
+    },
   },
 };
