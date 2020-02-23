adfbc033de3ecffd48b5542c8e3e1deb0d52c243
piotr-s <piotr.slawinski1@gmail.com>
2020-02-23 23:51:31 +0100

    Add public path

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,7 @@ module.exports = {
   devServer: {
     proxy: 'https://opensky-network.org/',
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/chemtrails-guru/' : '/',
   /*
    * Issue with peer dependencies required to make an alias to point to a single
    * leaflet root instance as in https://github.com/jurb/vue2-leaflet-heatmap/issues/1
