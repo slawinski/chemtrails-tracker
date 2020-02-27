8d211ca3f7d57f3bc4a0e2c0f6aa71d7407ec841
piotr-s <piotr.slawinski1@gmail.com>
2020-02-27 23:53:04 +0100

    Test for component mounting

--- src/components/Map.spec.js
+++ src/components/Map.spec.js
@@ -1,9 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
 import Map from './Map';
 
-describe('Map component', function() {
-  it('should render', function() {
+describe('Map component', () => {
+  it('should render', () => {
     const wrapper = shallowMount(Map);
-    expect(wrapper.vm).toBeCalled();
+
+    expect(wrapper.isVueInstance()).toBeTruthy();
+    expect(wrapper.is(Map)).toBe(true);
+    expect(wrapper.contains('div')).toBe(true);
   });
 });
