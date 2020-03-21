996919f41796015c72e732c7a4b3dffe21b059d1
piotr-s <piotr.slawinski1@gmail.com>
2020-03-21 22:07:21 +0100

    Refactor component wrapper

--- src/tests/Map.spec.js
+++ src/tests/Map.spec.js
@@ -1,10 +1,18 @@
 import { shallowMount } from '@vue/test-utils';
 import Map from '../components/Map';
 
+let wrapper = null;
+
+beforeEach(() => {
+  wrapper = shallowMount(Map);
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
 describe('Map component', () => {
   it('should render', () => {
-    const wrapper = shallowMount(Map);
-
     expect(wrapper.isVueInstance()).toBeTruthy();
     expect(wrapper.is(Map)).toBe(true);
     expect(wrapper.findAll({ ref: 'map' })).toHaveLength(1);
